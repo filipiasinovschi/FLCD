bfa877db7878a34e7475e70beefc6c916bcbf6f4
filipiasinovschi <72267969+filipiasinovschi@users.noreply.github.com>
2022-10-25 16:51:50 +0300

    Add files via upload

---
+++ LAB2/LAB2/src/Main.java
@@ -0,0 +1,28 @@
+import domain.SymbolTable;
+
+public class Main {
+    public static void main(String[] args) {
+        SymbolTable symbolTable = new SymbolTable(8);
+        String[] symbols = {"a1", "1a", "b2", "c3", "d4", "a1"};
+
+        System.out.println("Add --------------");
+        for (int i = 0; i < symbols.length; ++i) {
+            System.out.println(symbolTable.addSymbol(symbols[i]));
+        }
+
+        System.out.println("\nContains --------------");
+        for (int i = 0; i < symbols.length; ++i) {
+            System.out.println(symbolTable.containsSymbol(symbols[i]));
+        }
+
+        symbols = new String[]{"a1", "1a", "b2", "c3", "d4", "a1", "b5"};
+        System.out.println("\nPosition --------------");
+        for (int i = 0; i < symbols.length; ++i) {
+            System.out.println(symbolTable.getPosition(symbols[i]));
+        }
+
+        System.out.println("\nRemove --------------");
+        for (int i = 0; i < symbols.length; ++i) {
+            System.out.println(symbolTable.removeSymbol(symbols[i]));
+        }
+    }}
